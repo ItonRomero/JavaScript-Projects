9e9ce3bbdb66b524cae03ba9c4396b459be7b569
ItonRomero <jaysonromero1218@gmail.com>
2022-10-17 21:11:58 +0800

    updated color flipper

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -1,15 +1,25 @@
+
+// Initialize colors array 
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// Retrive button and span element 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+//Event Listener
 btn.addEventListener("click", function () {
+  //accesing random number functions below
   const randomNumber = getRandomNumber();
-  // console.log(randomNumber);
 
+  //Targeting body of DOM using document.body
+  //Update body using style.backgroundColor
+  // accesing arrays using colors[randomNumber];
   document.body.style.backgroundColor = colors[randomNumber];
+  // updating content of color DOM 
   color.textContent = colors[randomNumber];
 });
 
+// function for to have randomnumber
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
 }
